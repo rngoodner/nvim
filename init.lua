367b61693a8b84e551687490a68fdaf450cf9625
Ryan Goodner <ryangoodner@pm.me>
2022-09-14 22:48:26 -0600

    remove indentation guide and treesitter which caused bad syntax highlighting

--- init.lua
+++ init.lua
@@ -35,8 +35,6 @@ require('packer').startup(function()
   use "mattn/vim-goimports"                 -- Go code formatting and imports management
   use "preservim/nerdcommenter"             -- Comments with <leader>cc, <leader>cu, <leader>c<space>
   use "kyazdani42/nvim-tree.lua"            -- file tree, toggle w/ C-n, manip w/ a,d,c,p,x,R
-  use "lukas-reineke/indent-blankline.nvim" -- indentation guide
-  use "nvim-treesitter/nvim-treesitter"     -- indent-blankline dependency
 end)
 
 -- recompile if this file changes
@@ -217,44 +215,3 @@ vim.cmd 'set number'
 -- highlight current line
 vim.cmd 'set cursorline'
 
--- indent blankline config
-require("indent_blankline").setup {
-  char="",
-  show_current_context = true,
-  show_current_context_start = true,
-}
-
--- treesitter config
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "go", "cpp", "python", "bash" },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  auto_install = true,
-
-  -- List of parsers to ignore installing (for "all")
-  --ignore_install = { "javascript" },
-
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    --disable = { "c", "rust" },
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-}
